9cb7306ceee16ff4439725ebd28ad568f93405cd
mixrecovery19 <michael.kalaf@gmail.com>
2025-03-21 07:18:21 +1100

    fixed footer location

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// ***IF NEEDED, ADD CODE HERE***
+// JavaScript for the inex.html page embedded video
+// assists in the lazy loading of the video
+// to ensure that the page loads faster
+document.addEventListener("DOMContentLoaded", function() {
+    var iframe = document.getElementById('youtube-iframe');
+    if ('loading' in HTMLIFrameElement.prototype) {
+        iframe.setAttribute('loading', 'lazy');
+    }
+});
