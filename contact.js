27df93a576bc611ee05e660d7dba13c7ae494f47
mixrecovery19 <michael.kalaf@gmail.com>
2025-03-21 06:38:46 +1100

    contact form alert

--- contact.js
+++ contact.js
@@ -1,1 +1,19 @@
 // ***IF NEEDED, ADD CODE HERE***
+function signUpForm() {
+    var form = document.querySelector("form");
+
+    var contactDetails = {
+        name: form.name.value,
+        email: form.email.value,
+        age: form.age.value,
+        contact1: (form.querySelector('input[name="contact1"]:checked') || {}).value || "No Selection",
+       
+    };
+
+    var contactRequestMessage = `Your Contact Name Is: ${contactDetails.name} 
+                Your email: ${contactDetails.email}
+                Your Age: ${contactDetails.age}
+                Contact Method: ${contactDetails.contact1}`;
+
+    alert(contactRequestMessage);
+}
